4433c5734ed0c9807cd270057a54d14e223f589d
Ryan <rom@ualberta.ca>
2024-06-13 13:13:09 -0600

    Added No wifi as a restricted ip address

--- globalVariables.py
+++ globalVariables.py
@@ -1,6 +1,6 @@
 programName = "IPLockPC"
 execIntervalInSeconds = 1
-restricted_ip_addresses = [{"Name":"Home 2.4G", "IP Address":"192.168.1.81"}]
+restricted_ip_addresses = [{"Name":"Home 2.4G", "IP Address":"192.168.1.81"}, {"Name":"No Wifi","IP Address":"127.0.0.1"}]
 iso_hour_start = 6
 iso_hour_end = 14
 path_to_exe_file = "C:\\Users\\ryanr\\IP-Lock-PC\\dist\\IPLockPC.exe"
