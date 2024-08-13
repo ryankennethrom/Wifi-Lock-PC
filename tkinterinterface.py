a57900f10b519b4a8dea42ec1bd089d6de2dc01d
ryan <rom@ualberta.ca>
2024-08-12 21:24:41 -0600

    add a listbox to the window for displaying restricted networks

--- tkinterinterface.py
+++ tkinterinterface.py
@@ -2,4 +2,15 @@ import tkinter as tk
 
 if __name__ == "__main__":
     root = tk.Tk()
+    root.update_idletasks()
+    width = root.winfo_width()
+    height = root.winfo_height()
+    root.title('WifiLockPC')
+    x = (root.winfo_screenwidth() // 2) - (width //2)
+    y = (root.winfo_screenheight() // 2) - (height //2)
+    root.geometry("{}x{}+{}+{}".format(width,height, x, y))
+    myLabel = tk.Label(root, text="Restricted Networks", anchor="w",justify="left")
+    myLabel.pack(padx=10, fill=tk.X)
+    my_listbox = tk.Listbox(root)
+    my_listbox.pack(padx=10, pady=5, fill=tk.X)
     root.mainloop()
