c9dae0fecc626e500d5e4a413f118d425e5ce276
ryan <rom@ualberta.ca>
2024-08-15 00:34:26 -0600

    add ui for entering SSID and button to add it as a restricted network

--- tkinterinterface.py
+++ tkinterinterface.py
@@ -1,16 +1,44 @@
 import tkinter as tk
 
+def entry_focus_in(event):
+    if entry1.get() == "Enter message...":
+        entry1.delete(0, 'end')
+        entry1.config(fg='black')
+
+def entry_focus_out(event):
+    if entry1.get() == "":
+        entry1.insert(0, "Enter message...")
+        entry1.config(fg="gray")
+
 if __name__ == "__main__":
     root = tk.Tk()
     root.update_idletasks()
-    width = root.winfo_width()
-    height = root.winfo_height()
+    width = 500
+    height = 300
     root.title('WifiLockPC')
+
     x = (root.winfo_screenwidth() // 2) - (width //2)
     y = (root.winfo_screenheight() // 2) - (height //2)
     root.geometry("{}x{}+{}+{}".format(width,height, x, y))
+    root.resizable(0,0)
+
     myLabel = tk.Label(root, text="Restricted Networks", anchor="w",justify="left")
     myLabel.pack(padx=10, fill=tk.X)
-    my_listbox = tk.Listbox(root)
+
+    my_listbox = tk.Listbox(root, bg='green')
+    # tk.Button(root, text="Button", command=
     my_listbox.pack(padx=10, pady=5, fill=tk.X)
+
+    frame1 = tk.Frame(root, height=20, bg="green")
+    frame1.pack(padx=10, pady=1, fill=tk.X)
+
+    entry1 = tk.Entry(frame1, width=60, bg="green")
+    # entry1.grid(column=0, row=1)
+    entry1.pack( side='left', fill=tk.X)
+
+    button1 = tk.Button(frame1,width=20, text="Restrict Network")
+    # button1.grid(column=1, row=1)
+    button1.pack(padx=5, side='left',fill=tk.X)
+
+
     root.mainloop()
