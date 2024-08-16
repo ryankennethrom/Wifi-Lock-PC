f1ef7ca81720739c4adf56812395df6340030d8e
ryan <rom@ualberta.ca>
2024-08-15 20:09:16 -0600

    add ability to insert items to list and scroll down

--- tkinterinterface.py
+++ tkinterinterface.py
@@ -1,14 +1,11 @@
 import tkinter as tk
 
-def entry_focus_in(event):
-    if entry1.get() == "Enter message...":
-        entry1.delete(0, 'end')
-        entry1.config(fg='black')
+# def restrictNetworkButton():
+#     my_listbox.get(my_listbox.curselection())
 
-def entry_focus_out(event):
-    if entry1.get() == "":
-        entry1.insert(0, "Enter message...")
-        entry1.config(fg="gray")
+def add():
+    my_listbox.insert(my_listbox.size(), entry1.get())
+    entry1.delete(0, 'end')
 
 if __name__ == "__main__":
     root = tk.Tk()
@@ -25,20 +22,31 @@ if __name__ == "__main__":
     myLabel = tk.Label(root, text="Restricted Networks", anchor="w",justify="left")
     myLabel.pack(padx=10, fill=tk.X)
 
-    my_listbox = tk.Listbox(root, bg='green')
+    frame0 = tk.Frame(root, height=20, bg="green")
+    frame0.pack(padx=10, pady=1, fill=tk.X)
+
+    my_listbox = tk.Listbox(frame0, width=72,bg='green')
     # tk.Button(root, text="Button", command=
-    my_listbox.pack(padx=10, pady=5, fill=tk.X)
+    my_listbox.pack(padx=10, pady=5, side="left", fill=tk.BOTH)
+
+    scrollbar = tk.Scrollbar(frame0)
+    scrollbar.pack(side = 'left', pady=5, fill=tk.BOTH)
+    
+    # Attach Scrollbar to listbox
+    my_listbox.config(yscrollcommand = scrollbar.set)
+
+    scrollbar.config(command=my_listbox.yview)
 
     frame1 = tk.Frame(root, height=20, bg="green")
     frame1.pack(padx=10, pady=1, fill=tk.X)
 
     entry1 = tk.Entry(frame1, width=60, bg="green")
     # entry1.grid(column=0, row=1)
-    entry1.pack( side='left', fill=tk.X)
+    entry1.pack( side='left', padx=10, fill=tk.X)
 
-    button1 = tk.Button(frame1,width=20, text="Restrict Network")
+    button1 = tk.Button(frame1,width=20, text="Restrict Network", command=add)
     # button1.grid(column=1, row=1)
-    button1.pack(padx=5, side='left',fill=tk.X)
+    button1.pack( side='left',fill=tk.X)
 
 
     root.mainloop()
